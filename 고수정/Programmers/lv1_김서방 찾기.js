f8ad80d83fc0495ddc3c595abd5fcbabf114247e
sujeong-ko <dev.sjko@gmail.com>
2022-10-07 01:54:20 +0900

    lv1 문제 풀이

---
+++ 고수정/Programmers/lv1_김서방 찾기.js
@@ -0,0 +1,5 @@
+function solution(seoul) {
+    var index = seoul.findIndex((x)=> x==='Kim');
+    console.log(index);
+    return `김서방은 ${index}에 있다`;
+}   
--- 고수정/Programmers/lv1_문자열 정렬하기.js
+++ 고수정/Programmers/lv1_문자열 정렬하기.js
@@ -1,15 +1,14 @@
 function solution(strings, n) {
     var answer = [];
     const arr = [] ;
-    const map = strings.map((str) => str.slice(n, n+1)); 
+    const map = strings.sort().map((str) => str.slice(n, n+1)); 
     // 비교할 글자 배열로 만들기
     for(let i=0; i < strings.length; i++){
         arr.push([strings[i], map[i]])
     }
-    console.log(arr);
-    // arr = [['sun', 'u'], ['bed', 'e'], ['car', 'a']]
+
     arr.sort((a, b) => {
-        let diff = a[1].charCodeAt() - b[1].charCodeAt();
+        let diff = a[1].charCodeAt() - b[1].charCodeAt()
         if(diff > 0){
             return diff;
         }
@@ -20,8 +19,9 @@ function solution(strings, n) {
             return 0;
         }
 })
-
-for(let x of arr){
+    console.log(arr);
+    
+    for(let x of arr){
     answer.push(x[0]);
 }
 
