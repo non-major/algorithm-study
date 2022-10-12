e0374f27476543634252d772cb2fe5ab823afebd
JiYun <jo522115@gmail.com>
2022-10-12 10:13:27 +0900

    3주차 수정

--- 김지윤/Programmers/Programmerslv.2_learn_courses_30_lessons_12951.js
+++ 김지윤/Programmers/Programmerslv.2_learn_courses_30_lessons_12951.js
@@ -19,3 +19,8 @@ function solution(s) {
 // function solution(s) {
 //     return s.split(" ").map(v => v.charAt(0).toUpperCase() + v.substring(1).toLowerCase()).join(" ");
 // }
+
+/* 회고
+index 0으로 바로 접근한 것을 charAt(0)으로 바꿔 접근하니 런타임 에러가 발생하지 않았다.
+검색 결과 시간 차이가 나는 이유는 알 수 없었지만 index로 직접 접근하는 것이 가독성이 떨어지고(코드 내에서 배열 접근과 혼동될 수 있음)
+일부 브라우저에서 지원하지 않는다고 한다. 앞으로 개별문자 접근할 때는 charAt을 사용하는 습관을 들여야겠다! */
